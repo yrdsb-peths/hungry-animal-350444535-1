64679ca0a72753f79ad890cedf107e7c75e2fb8c
350444535 <150150007+350444535@users.noreply.github.com>
2024-09-19 13:15:10 -0400

    part 1 part 2

--- Elephant.java
+++ Elephant.java
@@ -15,5 +15,25 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        eat();
+    }
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
     }
 }
