467b02315a470014fe7ca4fdca7a7c25495dcf1d
350444535 <150150007+350444535@users.noreply.github.com>
2024-09-24 07:28:39 -0400

    Part 2 Part 1

--- Elephant.java
+++ Elephant.java
@@ -8,6 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootImage[] idle = new GreenfootImage[8];
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
